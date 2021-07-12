439f00a738e6087384f8d9b153106220d7ac2746
XWANG270 <xwang270@ford.com>
2021-07-12 08:38:30 +0800

    first connect

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {SignalService} from "./signal.service";
+import * as signalR from '@microsoft/signalr';
+import {environment} from "../environments/environment";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'SignalRTest';
+
+
+  constructor(private service: SignalService) {
+    // this.title = title;
+
+  }
+
+  ngOnInit(): void {
+    this.service.getWeather().subscribe(res => {
+      console.log(res);
+    })
+
+    const connection = new signalR.HubConnectionBuilder()
+      .configureLogging(signalR.LogLevel.Information)
+      .withUrl(environment.baseUrl + '/chatHub')
+      .build();
+
+    connection.start().then(function () {
+      console.log('SignalR Connected!');
+      setTimeout(() => {
+        connection.invoke("SendMessage", 'xinyu', 'hello signalR').catch(function (err) {
+          return console.error(err.toString());
+        });
+      }, 5000);
+    }).catch(function (err) {
+      return console.error(err.toString());
+    });
+
+    connection.on("ReceiveMessage", function (user, message) {
+      console.log('receive message' + user + message);
+    });
+  }
+
+
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,10 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
-
+export const webServerUrl = '/posts';
 export const environment = {
   production: false,
-  baseUrl: 'sfdsfdfdsa'
+  baseUrl: `${webServerUrl}`
 };
 
 /*
